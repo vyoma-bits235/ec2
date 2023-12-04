801daded59987bedcd36d03866dce38d5ee750cf
vyoma-bits235 <f20230831@pilani.bits-pilani.ac.in>
2023-12-05 03:40:51 +0530

    h

--- poll/urls.py
+++ poll/urls.py
@@ -1,35 +1,12 @@
-"""poll URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from questions.views import (question_detail , 
-save_question_result ,login_view , register_view ,
- dashboard ,create_poll , see_answers)
-
-
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from questions import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('' , login_view , name="login"),
-    path('register/' , register_view , name="register_view"),
-    path('dashboard/' , dashboard , name="dashboard"),
-    path('create_poll/' , create_poll , name="create_poll"),
-path('see_answers/' , see_answers , name="see_answers"),
-
-
-
-    path('question/<question_uid>/' , question_detail , name="question_detail"),
-    path('api/save_question_result/' , save_question_result),
-    path('admin/', admin.site.urls),
-]
+   
+    
+    path('questions/',include('questions.urls')),
+    path("admin/", admin.site.urls),
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- questions/admin.py
+++ questions/admin.py
@@ -1,23 +1,16 @@
-import re
 from django.contrib import admin
 
 # Register your models here.
-
-from .models import Question , Answers
-
-
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ['question_text' , 'user' ]
-
-
-@admin.register(Answers)
-class AnswerAdmin(admin.ModelAdmin):
-    list_display = ['answer_text' ,'question_text' ,'counter' ]
-
-
-    def question_text(self , obj):
-        return obj.question.question_text
-
-
-
+from .models import MenuItem, Rating, Orders,Image23,Attendance,AT2,Menu,ExcelFile,ExcelData,Expenditure,Items,UserProfile
+admin.site.register(MenuItem)
+admin.site.register(Rating)
+admin.site.register(Orders)
+admin.site.register(Image23)
+admin.site.register(Attendance)
+admin.site.register(AT2)
+admin.site.register(Menu)
+admin.site.register(ExcelFile)
+admin.site.register(ExcelData)
+admin.site.register(Expenditure)
+admin.site.register(Items)
+admin.site.register(UserProfile)
